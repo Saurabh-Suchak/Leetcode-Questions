7535ea85bcba911d931515a449ca0d0302d06bf4
Saurabh Suchak <91744743+Saurabh-Suchak@users.noreply.github.com>
2023-07-15 20:19:56 +0530

    Time: 459 ms (10.69%), Space: 98.7 MB (6.94%) - LeetHub

--- 2580-count-ways-to-group-overlapping-ranges/2580-count-ways-to-group-overlapping-ranges.cpp
+++ 2580-count-ways-to-group-overlapping-ranges/2580-count-ways-to-group-overlapping-ranges.cpp
@@ -17,20 +17,6 @@ public:
         for(long long int i=1;i<=n;i++){
             count=(count*2)%(mod);
         }return count;
-        // long long i=0,n=v.size(),mod=1e9+7;
-        // sort(v.begin(),v.end());
-        // int mergesize=1; // size=1 because we started to iterate from i=1 i.e. second interval.
-        // long long ans=1; // distributing the first interval in 2 ways
-        // //int first = v[0][0];
-        // int second = -1;
-        // for(i=0;i<n;i++){
-        //     if(v[i][0]<=second)second = max(second,v[i][1]);
-        //     else{
-        //         mergesize++;
-        //         //first=v[i][0]; (incase,if u want to store the merged intervals)
-        //         second=v[i][1];
-        //         ans=(ans*2)%mod;  // distributing the current unoverlapped interval we got either in group 1 or in group 2.
-        //     }
-        // }return ans;
+        
     }
 };
