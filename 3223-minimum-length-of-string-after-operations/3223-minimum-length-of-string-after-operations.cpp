f839856de0e4443797de789328aa84c84a432bed
Saurabh Suchak <91744743+Saurabh-Suchak@users.noreply.github.com>
2024-08-21 04:43:36 +0530

    Time: 152 ms (65.79%), Space: 32 MB (43.91%) - LeetHub

---
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    
+    // substr(pos,length) //erase (pos,length)
+    int minimumLength(string s) {
+       
+        unordered_map<char,int>mp;
+        for(int i=0;i<s.size();i++){
+            mp[s[i]]++;
+        }
+        int count  =0;
+        
+        for(auto &i:mp){
+            if(i.second%2==0){
+                count = count + 2;
+                continue;
+            }
+            count = count + 1;
+        }
+        return count;
+        
+    }
+};
