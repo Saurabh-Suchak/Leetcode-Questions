dabb5e956be62cfee25ac18f3ea1bdc2dd7abeef
Saurabh Suchak <91744743+Saurabh-Suchak@users.noreply.github.com>
2024-11-05 12:13:26 -0800

    Time: 0 ms (100.00%), Space: 16.3 MB (80.58%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
     string largestOddNumber(string num) {
-        int n = num.size();
-        for(int i=n-1; i>=0; i--){
-            if((num[i]-'0')%2 !=0) return num.substr(0, i+1);
+        string ans = "";
+        long long int ind = -1;
+        for(long long int i=num.size()-1;i>=0;i--){
+            if((num[i]-'0')%2 != 0){
+                ind = i;
+                break;
+            }
         }
-        return "";
+        if(ind == -1){
+            return "";
         }
-    
+        
+        return num.substr(0,ind+1);
+    }
 };
