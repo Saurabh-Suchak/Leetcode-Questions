a2f8cc9953fdeb76eb49c596f6c8890b278d35a4
Saurabh Suchak <91744743+Saurabh-Suchak@users.noreply.github.com>
2024-11-24 09:51:49 +0530

    Time: 3 ms (45.61%), Space: 9.9 MB (58.08%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,39 +1,56 @@
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//         string ans;string temp ="";
+//         for(int i=s.size()-1;i>=0;i--){
+//             if(s[i]==' '){
+//                 while(s[i]==' '){
+//                     i--;
+//                 }
+//                 i++;
+//                 reverse(temp.begin(),temp.end());
+//                 ans= ans + temp + " ";
+//                 temp = "";
+//                 continue;
+//             }
+            
+//             temp=temp+s[i];
+//         }
+//         reverse(temp.begin(),temp.end());
+//         ans=ans+temp;
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     string reverseWords(string s) {
-        string ans;string temp = "";
-        int i = s.size()-1;
-        while(s[i]==' '){
-            i--;
-        }
-        int t = 0;
-        while(s[t]==' '){
-            t++;
-        }
-        int last = 0;
-        for(i;i>=t;i--){
-            
-            if(last == 1 && s[i]==' '){
-                continue;
-            }
-            if(s[i]!=' '){
-                last = 0;
-            }
-           
-            if(s[i]==' '){
-                last =1;
-                
-                 reverse(temp.begin(),temp.end());
-            
-                ans=ans+temp+" ";
-                temp="";continue;
-               
+        string ans;
+        string temp = "";
+        int i = s.size() - 1;
+
+        while (i >= 0) {
+            // Skip trailing spaces
+            while (i >= 0 && s[i] == ' ') {
+                i--;
             }
             
-            temp = temp + s[i];
-        }
-        reverse(temp.begin(),temp.end());
-                ans=ans+temp;
-                return ans;
+            // Collect a word
+            temp = "";
+            while (i >= 0 && s[i] != ' ') {
+                temp = s[i] + temp; // Reverse while collecting
+                i--;
             }
+            
+            // Append the word if it's not empty
+            if (!temp.empty()) {
+                if (!ans.empty()) {
+                    ans += " ";
+                }
+                ans += temp;
+            }
+        }
+
+        return ans;
+    }
 };
